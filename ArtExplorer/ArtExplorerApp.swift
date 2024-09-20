a4a305bd00184a1c4a4113a73e660dc9dfb16241
Allen Odoom <allencvb@gmail.com>
2024-09-20 15:48:25 -0400

    Initial Commit

---
+++ ArtExplorer/ArtExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtExplorerApp.swift
+//  ArtExplorer
+//
+//  Created by Allen Odoom on 9/20/24.
+//
+
+import SwiftUI
+
+@main
+struct ArtExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArtExplorer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ArtExplorer
+//
+//  Created by Allen Odoom on 9/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
